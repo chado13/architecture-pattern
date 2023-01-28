1eb21e14c9416ac9336e1b24b15ec085dd26dc0c
chado13 <geaya13@gmail.com>
2023-01-28 22:26:48 +0900

    test: api 및 service test 추가

--- tests/test_allocate.py
+++ tests/test_allocate.py
@@ -1,12 +1,18 @@
-
 import datetime
-from model import Batches, OrderLine, allocate, OutOfStock
+
 import pytest
+
+from app.domain.model import Batches, OrderLine, OutOfStock, allocate
 
 
 def test_prefers_current_stock_batchs_to_shipment():
     in_stock_batch = Batches("in-stock-batch", "retro-clock", 100, eta=None)
-    shipment_batch = Batches("shipment-batch", "retro-clock", 100, eta=datetime.datetime.today() + datetime.timedelta(days=1))
+    shipment_batch = Batches(
+        "shipment-batch",
+        "retro-clock",
+        100,
+        eta=datetime.datetime.today() + datetime.timedelta(days=1),
+    )
     line = OrderLine("oref", "retro-clock", 10)
 
     allocate(line, [in_stock_batch, shipment_batch])
@@ -14,10 +20,21 @@ def test_prefers_current_stock_batchs_to_shipment():
     assert in_stock_batch.available_qty == 90
     assert shipment_batch.available_qty == 100
 
+
 def test_prefers_earlier_batchs():
     earlist = Batches("speedy-batch", "minimalist-spoon", 100, eta=datetime.datetime.today())
-    medium = Batches("normal-batch", "minimalist-spoon", 100, eta=datetime.datetime.today() + datetime.timedelta(days=1))
-    latest = Batches("slow-batch", "minimalist-spoon", 100, eta=datetime.datetime.today()+datetime.timedelta(days=2))
+    medium = Batches(
+        "normal-batch",
+        "minimalist-spoon",
+        100,
+        eta=datetime.datetime.today() + datetime.timedelta(days=1),
+    )
+    latest = Batches(
+        "slow-batch",
+        "minimalist-spoon",
+        100,
+        eta=datetime.datetime.today() + datetime.timedelta(days=2),
+    )
     line = OrderLine("order1", "minimalist-spoon", 10)
 
     allocate(line, [medium, earlist, latest])
@@ -26,12 +43,18 @@ def test_prefers_earlier_batchs():
     assert medium.available_qty == 100
     assert latest.available_qty == 100
 
+
 def test_returns_allocated_batchs_id():
     in_stock_batch = Batches("in-stock-batch-id", "highbrow-poster", 100, eta=None)
-    shipment_batch = Batches("shipment-batch-id", "highbrow-poster", 100, eta=datetime.datetime.today() + datetime.timedelta(days=1))
+    shipment_batch = Batches(
+        "shipment-batch-id",
+        "highbrow-poster",
+        100,
+        eta=datetime.datetime.today() + datetime.timedelta(days=1),
+    )
     line = OrderLine("oref", "highbrow-poster", 10)
     allocation = allocate(line, [in_stock_batch, shipment_batch])
-    assert allocation == in_stock_batch.id
+    assert allocation == in_stock_batch.ref
 
 
 def test_raises_out_of_stock_exception_if_cannot_allocate():
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,14 +1,12 @@
-
-import model
-from app.adapter.repository import  SqlalchemyRepository
-
+import app.domain.model as model
+from app.adapter.repository import SqlalchemyRepository
 
 
 def test_repository_can_save_a_batch(session):
     # given
     batch = model.Batches("batch1", "rusty-soapdish", 100, eta=None)
     repo = SqlalchemyRepository(session)
-    
+
     # when
     repo.add(batch)
     session.commit()
@@ -16,6 +14,7 @@ def test_repository_can_save_a_batch(session):
     # then
     rows = list(session.execute("SELECT ref, sku, _purchased_qty, eta FROM batches"))
     assert rows == [("batch1", "rusty-soapdish", 100, None)]
+
 
 def test_repository_can_retrieve_a_batch_with_allocations(session):
     orderline_id = insert_order_line(session)
@@ -34,32 +33,32 @@ def test_repository_can_retrieve_a_batch_with_allocations(session):
         model.OrderLine("order1", "generic-sofa", 12),
     }
 
+
 def insert_order_line(session):
     session.execute(
-        'insert into order_lines (order_id, sku, qty)'
-        'values ("order1", "generic-sofa", 12)'
+        "insert into order_lines (order_id, sku, qty)" 'values ("order1", "generic-sofa", 12)'
     )
     [[orderline_id]] = session.execute(
-        'select id from order_lines where order_id=:order_id and sku=:sku',
-        dict(order_id="order1", sku="generic-sofa")
+        "select id from order_lines where order_id=:order_id and sku=:sku",
+        dict(order_id="order1", sku="generic-sofa"),
     )
     return orderline_id
 
+
 def insert_batch(session, batch_id):
     session.execute(
-        'insert into batches (ref, sku, _purchased_qty, eta) '
+        "insert into batches (ref, sku, _purchased_qty, eta) "
         'values (:batch_id, "generic-sofa", 100, null)',
-        dict(batch_id=batch_id)
+        dict(batch_id=batch_id),
     )
     [[batch_id]] = session.execute(
-        'select id from batches where ref=:batch_id and sku="generic-sofa"',
-        dict(batch_id=batch_id)
+        'select id from batches where ref=:batch_id and sku="generic-sofa"', dict(batch_id=batch_id)
     )
     return batch_id
 
+
 def insert_allocation(session, orderline_id, batch_id):
     session.execute(
-        'insert into allocations (orderline_id, batch_id) '
-        'values (:orderline_id, :batch_id)',
-        dict(orderline_id=orderline_id, batch_id=batch_id)
+        "insert into allocations (orderline_id, batch_id) " "values (:orderline_id, :batch_id)",
+        dict(orderline_id=orderline_id, batch_id=batch_id),
     )
